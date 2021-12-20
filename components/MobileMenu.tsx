d1bd875fa6fd2733c19fdd5f3bc9bb6dc098fee0
Omkar <omkarak45@gmail.com>
2021-12-20 23:30:03 +0530

    remove: removed newsletter from mobilemenu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -81,14 +81,7 @@ export default function MobileMenu() {
               <a className="flex w-auto pb-4">Snippets</a>
             </Link>
           </li>
-          <li
-            className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
-            style={{ transitionDelay: '300ms' }}
-          >
-            <Link href="/newsletter">
-              <a className="flex w-auto pb-4">Newsletter</a>
-            </Link>
-          </li>
+
           <li
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '325ms' }}
