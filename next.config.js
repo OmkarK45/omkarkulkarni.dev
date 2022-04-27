1725d797f18d6904786db89d42ce5bff1735f6c1
Omkar <omkarak45@gmail.com>
2022-04-27 22:50:13 +0530

    feat: lp3 redirect added

--- next.config.js
+++ next.config.js
@@ -32,6 +32,12 @@ module.exports = withContentlayer()({
         source: '/resume',
         destination: '/Omkar_Kulkarni_7387710594_Resume.pdf',
         permanent: true
+      },
+      {
+        source: '/lp3',
+        destination:
+          'https://www.notion.so/okayish/LP3-a3917501ab36460c8b304ef2e08415bf',
+        permanent: true
       }
     ];
   }
