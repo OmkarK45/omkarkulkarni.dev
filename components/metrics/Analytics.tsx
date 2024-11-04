0e918a8a1c1f0515edbe78eaafec3641114244f9
Omkar <omkarak45@gmail.com>
2024-11-04 23:10:29 +0530

    fix: fixed domains

--- components/metrics/Analytics.tsx
+++ components/metrics/Analytics.tsx
@@ -8,7 +8,7 @@ export default function AnalyticsCard() {
   const { data } = useSWR<Views>('/api/views', fetcher);
 
   const pageViews = new Number(data?.total);
-  const link = 'https://omkarkulkarni.tech';
+  const link = 'https://omkarkulkarni.vercel.app';
 
   return (
     <MetricCard
